eacae45550981aa951ffa95a8de5f15ef00754da
HerikHenrique <herikcontato@gmail.com>
2023-03-27 19:53:15 -0300

    Adição dos Cards!

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,32 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Menu from './components/Menu';
+import { display, grid, height, margin } from '@mui/system';
 
 const meuApp = {
-  backgroundColor: 'yellow',
+  height: 980,
 }
 
 
 function App() {
   return (
     <div className="App" style={meuApp}>
+    <Grid container>
 
-      <Header/>
 
-      <Grid container marginTop={5} >
+      <Grid item xs={12} sm={12} md={12} xl={12}>
+        <Header/>
+      </Grid>
 
-        <Grid item >
+      <Grid item 
+      sx={{
+        display: 'grid',
+        margin: "20px 10px"
+      }}>
         <Menu/>
-        </Grid>
+      </Grid>
 
-        <Grid>
-
-      <Card />
-        </Grid>
-
-      </Grid> 
-      <Test />
-
-    
+    </Grid>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,34 +12,9 @@ import { Grid } from '@mui/material';
 export default function MediaCard() {
   return (
     <div>
-      <Grid container >
-          
-        <Grid item xs={12} sm={3} md={3} lg={3}>
+      <Grid container spacing={2} marginLeft={2} >
 
-
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image="/static/images/cards/contemplative-reptile.jpg" // imagens dos pokemons que será carregada do banco de dados
-          title="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Aqui fica o primeiro parágrafo onde descreverei o tipo e elementos dos pokemons
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-      </Grid>
-
-
-      <Grid item xs={12} sm={3} md={3} lg={3}>
+      <Grid item xs={12} sm={3} md={3} lg={3} xl={3}>
       <Card sx={{ maxWidth: 345 }}>
         <CardMedia
           sx={{ height: 140 }}
@@ -62,7 +37,7 @@ export default function MediaCard() {
       </Card>
       </Grid> 
 
-      <Grid item xs={12}sm={3} md={3} lg={3}>
+      <Grid item xs={12}  sm={3} md={3} lg={3} xl={3}>
       <Card sx={{ maxWidth: 345 }}>
         <CardMedia
           sx={{ height: 140 }}
@@ -85,29 +60,58 @@ export default function MediaCard() {
       </Card>
       </Grid> 
 
-      <Grid item xs={12} sm={3} md={3} lg={3}>
+      <Grid item xs={12} sm={3} md={3} lg={3} xl={3}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg" // imagens dos pokemons que será carregada do banco de dados
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
 
+      
+      </Grid> 
 
-<Card sx={{ maxWidth: 345 }}>
-  <CardMedia
-    sx={{ height: 140 }}
-    image="/static/images/cards/contemplative-reptile.jpg" // imagens dos pokemons que será carregada do banco de dados
-    title="green iguana"
-  />
-  <CardContent>
-    <Typography gutterBottom variant="h5" component="div">
-      Lizard
-    </Typography>
-    <Typography variant="body2" color="text.secondary">
-      Aqui fica o primeiro parágrafo onde descreverei o tipo e elementos dos pokemons
-    </Typography>
-  </CardContent>
-  <CardActions>
-    <Button size="small">Share</Button>
-    <Button size="small">Learn More</Button>
-  </CardActions>
-</Card>
-</Grid>
+      <Grid item xs={12} sm={3} md={3} lg={3} xl={3}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg" // imagens dos pokemons que será carregada do banco de dados
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+
+      
+      </Grid> 
+
+      
+
 
       </Grid>
 
